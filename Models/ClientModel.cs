7b29a1fbc970a780f6aa72840dce9001f34890ba
Stasy <stasysamdance@gmail.com>
2020-03-25 10:11:59 +0300

    first commit

---
+++ Models/ClientModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterDetailApplication.Models
+{
+    public class ClientModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        
+        public DateTime DateOfBirth { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ Models/OrderModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterDetailApplication.Models
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public int Sum { get; set; }
+    }
+}
