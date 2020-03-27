cafb15be2f7cf249efb0226181e79abb466b892c
Stasy <stasysamdance@gmail.com>
2020-03-27 17:45:20 +0300

    Models created

--- MasterDetail.DBRepository/Models/Client.cs
+++ MasterDetail.DBRepository/Models/Client.cs
@@ -1,15 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace MasterDetail.Host.Models
+namespace MasterDetail.DBRepository.Models
 {
-    public class ClientModel
+    public class Client
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Patronymic { get; set; }
-        
         public DateTime DateOfBirth { get; set; }
         public string Phone { get; set; }
+
+        public virtual ICollection<Order> Orders { get; private set; }
+        public Client()
+        {
+            Orders = new List<Order>();
+        }
     }
+
 }
--- MasterDetail.DBRepository/Models/Order.cs
+++ MasterDetail.DBRepository/Models/Order.cs
@@ -1,6 +1,6 @@
-﻿namespace MasterDetail.Host.Models
+﻿namespace MasterDetail.DBRepository.Models
 {
-    public class OrderModel
+    public class Order
     {
         public int Id { get; set; }
         public int Sum { get; set; }
